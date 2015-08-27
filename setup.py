943a0a6586c7f9ff1c183da30fe0c4f73f1ac561
Tom <tom@tomforb.es>
2015-08-27 19:31:59 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cyborg',
+    version='',
+    packages=['cyborg'],
+    url='',
+    license='',
+    author='Orf',
+    author_email='tom@tomforb.es',
+    description='',
+    install_requires=[
+        "aio.pipes",
+        "aiohttp"
+    ]
+)
